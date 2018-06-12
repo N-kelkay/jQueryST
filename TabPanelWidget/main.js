996f61349f8ad20ffcdfbce40674f6445e4d3b7e
Natenael Kelkay <natikelkay@gmail.com>
2018-06-12 16:50:16 -0400

    finished building and coding panels

--- TabPanelWidget/main.js
+++ TabPanelWidget/main.js
@@ -1,12 +1,16 @@
 $(function () {
     $('.tab-panels .tabs li').on("click", function () {
-        $('.tab-panels .tabs li.active').removeClass('active');
+
+        //Put $ sign infront of variable if it uses jQuery (so the coder can know)
+        var $panel = $(this).closest(".tab-panels");
+
+        $panel.find('.tabs li.active').removeClass('active');
         $(this).addClass('active');
 
         //figure out which panel to show
         var panelToShow = $(this).attr('rel');
 
-        $('.tab-panels .panel.active').slideUp(200, showNextPanel);
+        $panel.find('.panel.active').slideUp(200, showNextPanel);
 
         function showNextPanel() {
             $(this).removeClass("active");
