0bdf15344c865fccf6b6094d8c5ea81b87890843
Natenael Kelkay <natikelkay@gmail.com>
2018-06-12 16:25:08 -0400

    made panels work

---
+++ TabPanelWidget/main.js
@@ -0,0 +1,17 @@
+$(function () {
+
+    $('.tab-panels .tabs li').on("click", function () {
+        $('.tab-panels .tabs li.active').removeClass('active');
+        $(this).addClass('active');
+
+        //figure out which panel to show
+        var panelToShow = $(this).attr('rel');
+
+        $('.tab-panels .panel.active').slideUp(300, function () {
+            $(this).removeClass("active");
+            $("#" + panelToShow).slideDown(300, function () {
+                $(this).addClass("active");
+            })
+        });
+    })
+})
