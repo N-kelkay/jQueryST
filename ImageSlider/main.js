f23bb63fc57e3e8623b35816ebb3a5476e993bea
Natenael Kelkay <natikelkay@gmail.com>
2018-06-13 09:05:16 -0400

    finished slides

--- ImageSlider/main.js
+++ ImageSlider/main.js
@@ -9,16 +9,43 @@
 
 $(function () {
 
+    //Configuration
     var width = 720;
     var animationSpeed = 1000;
     var pause = 3000;
+    var currentSlide = 1;
+
+    //Cache DOM
+    var $slider = $("#slider");
+    var $slideContainer = $slider.find(".slides");
+    var $slides = $slideContainer.find(".slide");
 
     //setInterval
-        //animate margin-left
-    setInterval(function () {
-        $("#slider .slides").animate({'margin-left': "-=" + width}, animationSpeed);
-    }, pause);
-        //if it's the last slide, go back to position 1 (0px)
+    //animate margin-left
+
+    var interval;
+
+    function startSlider() {
+        interval = setInterval(function () {
+            $slideContainer.animate({'margin-left': "-=" + width}, animationSpeed, function () {
+                //if it's the last slide, go back to position 1 (0px)
+                currentSlide++;
+                if (currentSlide == $(".slide").length) {
+                    currentSlide = 1;
+                    $slideContainer.css('margin-left', 0);
+                }
+            });
+            }, pause);
+    }
+
+    //Stops the slider
+    function stopSlider() {
+        clearInterval(interval);
+    }
+
     //listen for mouseenter and pause
     //resume on mouseleave
+    $slider.on('mouseenter', stopSlider).on('mouseleave', startSlider);
+
+    startSlider();
 })
