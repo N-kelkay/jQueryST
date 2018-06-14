731776b29a452606bf9d645593967ca31548e342
Natenael Kelkay <natikelkay@gmail.com>
2018-06-13 21:31:53 -0400

    added text for AJAX

--- AJAX1/main.js
+++ AJAX1/main.js
@@ -1,3 +1,21 @@
+/*
+AJAX: Asynchronous JavaScript And XML
+Use XMLHttpRequest (XHR) objects to interact with servers
+
+AJAX is a developer's dream, because you can:
+Update a web page without reloading the page
+Request data from a server - after the page has loaded
+Receive data from a server - after the page has loaded
+Send data to a server - in the background
+*/
+
+/*
+API: Application Programming Interface
+  software intermediary that allows two applications to talk to each other.
+  Each time you use an app like Facebook, send an instant message,
+  or check the weather on your phone, you’re using an API.
+ */
+
 $(function () {
     $.ajax({
         type: 'GET',
