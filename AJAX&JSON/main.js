7e72f1d25876984881d88bfdd82ca4f3bb5ef72e
Natenael Kelkay <natikelkay@gmail.com>
2018-06-15 17:36:57 -0400

    added JSON basic concepts

---
+++ AJAX&JSON/main.js
@@ -0,0 +1,6 @@
+/*
+ * AJAX: Downloads new data from a different URL
+ * JSON (Java Script Object Notation): Storing and working with data (sending, receiving and storing data)
+ *      - Objects and Arrays nested inside each other
+ */
+
