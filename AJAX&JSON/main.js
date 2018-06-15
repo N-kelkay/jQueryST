4726fdc155c67f641368857e2d4f3be4002fd9bb
Natenael Kelkay <natikelkay@gmail.com>
2018-06-15 17:59:11 -0400

    established 3 steps

--- AJAX&JSON/main.js
+++ AJAX&JSON/main.js
@@ -4,14 +4,22 @@
  *      - Objects and Arrays nested inside each other
  */
 
+//First step in connecting to server is to create a variable of XMLHttpRequest type
 //XMLHttpRequest: Web browsers have a built in tool called XMLHttpRequest.
 // This tool does the heavy lifting  of establishing a connection with the
 // desired URL to download data from, and then it lets the user send and receive data
 var ourRequest = new XMLHttpRequest();
 
+//Second Step is to request something
 //First Argument:
 //To receive data from the server type 'GET'
 //To send data to the server you type 'POST'
 //Second Argument:
 //Url to the server to receive or send data
 ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+
+//Third step is to write what to do once the data is sent or received
+//Self explanatory: on load what should the website do?
+ourRequest.onload = function () {
+
+}
