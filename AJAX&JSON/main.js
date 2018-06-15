f3aa83b1c24dd8fbc1ce3c0ec67b6997f3c892b2
Natenael Kelkay <natikelkay@gmail.com>
2018-06-15 17:52:10 -0400

    established the XMLHttpRequest type

--- AJAX&JSON/main.js
+++ AJAX&JSON/main.js
@@ -4,3 +4,14 @@
  *      - Objects and Arrays nested inside each other
  */
 
+//XMLHttpRequest: Web browsers have a built in tool called XMLHttpRequest.
+// This tool does the heavy lifting  of establishing a connection with the
+// desired URL to download data from, and then it lets the user send and receive data
+var ourRequest = new XMLHttpRequest();
+
+//First Argument:
+//To receive data from the server type 'GET'
+//To send data to the server you type 'POST'
+//Second Argument:
+//Url to the server to receive or send data
+ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
