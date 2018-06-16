9034e901f82e2c56ca79d90fc2bf11030f77c9fc
Natenael Kelkay <natikelkay@gmail.com>
2018-06-15 20:33:31 -0400

    successfuly hided button when all the urls are used up

--- AJAX&JSON/main.js
+++ AJAX&JSON/main.js
@@ -3,39 +3,32 @@ $(function () {
     $("#btn").on("click", function () {
         count++;
 
-            /*
-     * AJAX (Asynchronous JavaScript and XML): [Asynchronous:(in the background; not requiring refresh) JavaScript And XML:(data format very similar to JSON. We are using JSON instead of XML but XML sounds better with the acronym so... ya)]:
-      * Downloads new data from a different URL^
-      *
-     * JSON (Java Script Object Notation): Storing and working with data (sending, receiving and storing data)
-     *      - Objects and Arrays nested inside each other
-     */
+     //checks if the count is 3 (3 for 3 urls) if it exceeds the button will be hidden
+    if(count >=1 && count <=3) {
+        /*
+         * AJAX (Asynchronous JavaScript and XML): [Asynchronous:(in the background; not requiring refresh) JavaScript And XML:(data format very similar to JSON. We are using JSON instead of XML but XML sounds better with the acronym so... ya)]:
+          * Downloads new data from a different URL^
+          *
+         * JSON (Java Script Object Notation): Storing and working with data (sending, receiving and storing data)
+         *      - Objects and Arrays nested inside each other
+         */
 
-    //First step in connecting to server is to create a variable of XMLHttpRequest type
-    //XMLHttpRequest: Web browsers have a built in tool called XMLHttpRequest.
-    // This tool does the heavy lifting  of establishing a connection with the
-    // desired URL to download data from, and then it lets the user send and receive data
-    //BTW: technically It should be called JSONHttpRequest, because it is downloading JSON files. We no longer use XML files, but since XML was here for a long time it stuck :p
+        //First step in connecting to server is to create a variable of XMLHttpRequest type
+        //XMLHttpRequest: Web browsers have a built in tool called XMLHttpRequest.
+        // This tool does the heavy lifting  of establishing a connection with the
+        // desired URL to download data from, and then it lets the user send and receive data
+        //BTW: technically It should be called JSONHttpRequest, because it is downloading JSON files. We no longer use XML files, but since XML was here for a long time it stuck :p
         var ourRequest = new XMLHttpRequest();
 
-    //Second Step is to request something
-    //First Argument:
-    //To receive data from the server type 'GET'
-    //To send data to the server you type 'POST'
-    //Second Argument:
-    //Url to the server to receive or send data
-        if(count == 1) {
-            ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
-        }else if(count == 2){
-            ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-2.json');
-        } else if(count == 3){
-            ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-3.json');
-        }
-        else {
-            $("#btn").hide();
-        }
-    //Third step is to write what to do once the data is sent or received
-    //Self explanatory: on load what should the website do?
+        //Second Step is to request something
+        //First Argument:
+        //To receive data from the server type 'GET'
+        //To send data to the server you type 'POST'
+        //Second Argument:
+        //Url to the server to receive or send data
+        ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-' + count + '.json');
+        //Third step is to write what to do once the data is sent or received
+        //Self explanatory: on load what should the website do?
         ourRequest.onload = function () {
             //responseText is the text pulled from the server
             //The following method views the data receive as a giant text rather then a JSON file
@@ -46,10 +39,15 @@ $(function () {
             var ourData = JSON.parse(ourRequest.responseText);
             renderHTML(ourData);
         }
-    //After finished getting the files, send the files
-    //*If there is any error like : Failed to load resource: net::ERR_BLOCKED_BY_CLIENT
-    //*It might be because of an add block or some kind of firewall
+        //After finished getting the files, send the files
+        //*If there is any error like : Failed to load resource: net::ERR_BLOCKED_BY_CLIENT
+        //*It might be because of an add block or some kind of firewall
         ourRequest.send();
+
+    }else {
+        $("#btn").hide();
+    }
+
     })
 
     function renderHTML(data) {
